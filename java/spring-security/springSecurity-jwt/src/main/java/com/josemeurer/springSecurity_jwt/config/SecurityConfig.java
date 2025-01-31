a9ddf5176b1b18220bb0e5337b767d698446e83a
Jose Meurer <josewmeurer@gmail.com>
2025-01-31 13:07:11 -0300

    Adicionado a politica de STATELESS, e hierarquia de roles

--- java/spring-security/springSecurity-jwt/src/main/java/com/josemeurer/springSecurity_jwt/config/SecurityConfig.java
+++ java/spring-security/springSecurity-jwt/src/main/java/com/josemeurer/springSecurity_jwt/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.josemeurer.springSecurity_jwt.config;
 import com.josemeurer.springSecurity_jwt.security.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,9 +23,27 @@ public class SecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
+
+    /*
+        Maneira antiga, atualmente marcada como deprecated
+
+        @Bean
+        public RoleHierarchy roleHierarchy() {
+            RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+            String hierarchy = "ROLE_ADMIN > ROLE_USER";
+            roleHierarchy.setHierarchy(hierarchy);
+            return roleHierarchy;
+        }
+     */
+    @Bean
+    public static RoleHierarchy  roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_USER");
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
--- java/spring-security/springSecurity-jwt/src/main/resources/import.sql
+++ java/spring-security/springSecurity-jwt/src/main/resources/import.sql
@@ -5,5 +5,5 @@ INSERT INTO users(username, password) VALUES ('user', '$2a$10$PpOr85NrWJYx7Ww.iU
 INSERT INTO users(username, password) VALUES ('admin', '$2a$10$PpOr85NrWJYx7Ww.iUmv3uChQBggECfG4LoeaSd3TbYEbHUebvSAO');
 
 INSERT INTO user_roles(user_id, role_id) VALUES (1, 1);
-INSERT INTO user_roles(user_id, role_id) VALUES (2, 1);
+-- INSERT INTO user_roles(user_id, role_id) VALUES (2, 1);
 INSERT INTO user_roles(user_id, role_id) VALUES (2, 2);
