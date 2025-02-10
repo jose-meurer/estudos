6173f70e2e4258857af74b16f9c49e74492bb170
Jose Meurer <josewmeurer@gmail.com>
2025-02-10 17:29:22 -0300

    Estudo referencia javascript

---
+++ javascript/tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+const a = 12;
+const b = 12.5;
+const c = "José";
+const d = "a";
+const e = false;
+const f = { name: "José", age: 25 };
+const g = ["a", 15, false, "José"];
+const h = null;
+const i = undefined;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
