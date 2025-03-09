3fb2bc755e1bad690e16a91c5b56cef79b26f9d3
Jose Meurer <josewmeurer@gmail.com>
2025-03-08 21:03:04 -0300

    Estudo c#, tipo de dados, dateTime, nullable type e identificadores

---
+++ cSharp/Fundamentos/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Fundamentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              * Numeros
+              */
+
+            int idade = 27;
+            Console.WriteLine("int: " + idade);
+
+            double valor = 2.5654;
+            Console.WriteLine("double: " + valor); //precisao de 15~17 digitos
+
+            float temperatura = 27.43f;                 //Pode ser usado tanto f ou F
+            Console.WriteLine("float: " + temperatura); //precisao de 6~9 digitos
+
+            decimal medida = 27.43312312412312312312m; //Pode ser usado tanto m ou M
+            Console.WriteLine("decimal: " + medida); //precisao de 28~29 digitos
+
+            Console.WriteLine("Comparando a precisão de float, double e decimal");
+
+            float x = 1f / 3f;
+            double y = 1d / 3d;
+            decimal z = 1m / 3m;
+
+            Console.WriteLine("float: " + x);
+            Console.WriteLine("double: " + y);
+            Console.WriteLine("decimal: " + z);
+
+
+            /*
+             * Booleanos
+             */
+
+            bool status = true;
+            Console.WriteLine("booleano: " + status);
+
+            Console.WriteLine("Expressao booleana: " + (15 == 16));
+
+
+            /*
+             * Strings
+             */
+
+            //O tipo char pode expressar o caractere ou o valor unicode
+            char letra = 'n';
+            letra = '\u0051';
+            Console.WriteLine("char: " + letra);
+
+            //as strings são tipos de referência e são imutáveis
+            string nome = "Jose Meurer";
+            Console.WriteLine("string: " + nome);
+
+            //aqui estamos criando 3 strings distintas na memória, que posteriormente as não usadas serão coletadas pelo garbage colletor
+            string str = "Isto é uma string";
+            str = "Isto é uma string alterada";
+            Console.WriteLine($"string: {str}");
+
+            /*
+             * Dinamicos
+             */
+
+            // var e dynamic sao parecidos, porem var define o valor em tempo de compilacao, 
+            // enquanto dynamic é tratado em tempo de execucao
+
+            var rua = "Rua do João";
+            // rua = 19; //erro
+            Console.WriteLine("var: " + rua);
+
+            //o tipo dynamic tem um comportamento igual ao object, mas são resolvidos em tempo de execução(runtime)
+            dynamic cor = "Azul";
+            Console.WriteLine("dynamic: " + cor);
+            cor = 19; //funciona
+            Console.WriteLine("Novo tipo atribuido para o dynamic: " + cor);
+
+            Object obj = "Valor de Obj";
+            Console.WriteLine("Object: " + obj);
+
+            /*
+             * constante
+             */
+
+            //declaração de constante usa a palavra-chave const
+            const int valorReceber = 644;
+            //valorReceber = 888;
+
+            //não podemos alterar o valor de uma constante, o compilador vai indicar um erro
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ cSharp/NullableTypes/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("## Nullable Types ##\n");
+
+            Nullable<int> i = null;
+            Nullable<bool> b = null;
+            Nullable<double> d = null;
+            Nullable<float> f = null;
+
+            Console.WriteLine($"i={i}");
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"d={d}");
+            Console.WriteLine($"f={f}");
+
+            //Forma simplificada de Nullable
+            int? vari = null;
+            bool? varb = null;
+            double? vard = null;
+            float? varf = null;
+
+            Console.WriteLine($"i={vari}");
+            Console.WriteLine($"b={varb}");
+            Console.WriteLine($"d={vard}");
+            Console.WriteLine($"f={varf}");
+
+
+            int a1 = 100;
+            int? b1 = a1;
+            Console.WriteLine(b1);
+
+            int? a2 = null;
+            int b2 = a2 ?? 0; //Similar com curto-circuito do js, int != de int?, int? recebe int, mas int nao recebe int?
+            Console.WriteLine(b2);
+
+            int? x = 4;
+            int y = 3;
+            //int z = x * y;
+            //Console.WriteLine(z);
+
+            int? x1 = 4;
+            int y1 = 3;
+            int? z1 = x1 * y1; //A variavel que recebe a expressao tambem precisa ser Nullable
+            Console.WriteLine(z1);
+
+            //b3.Value, está retornando o valor subjacente do tipo T
+            int? b3 = 103;
+            if (b3.HasValue)
+            {
+                Console.WriteLine($"b3 is {b3.Value}");
+            }
+            else
+            {
+                Console.WriteLine("b3 does not have a value");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
