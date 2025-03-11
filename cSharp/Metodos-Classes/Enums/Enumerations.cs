95b12e6f0280ebb3dd260f1099d8005e080a45be
Jose Meurer <josewmeurer@gmail.com>
2025-03-10 22:03:20 -0300

    Estudo c#, enums, propriedades e structs

---
+++ cSharp/Metodos-Classes/Enums/Enumerations.cs
@@ -0,0 +1,24 @@
+﻿namespace Enums;
+
+enum Categorias
+{
+    Moda,
+    Automotivo,
+    Artes,
+    Bebidas,
+    Livros,
+    Brinquedos
+}
+
+enum DiasDaSemana
+{
+    Segunda = 10,
+    Terça = 12,
+    Quarta = 15,
+    Quinta = 17,
+    Sexta = 19,
+    Sábado = 21,
+    Domingo = 22
+}
+
+
---
+++ cSharp/Metodos-Classes/Strutcs/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("## Structs ##\n");
+
+        Cliente cliente = new();
+        cliente.Nome = "Maria";
+        cliente.Idade = 19;
+
+        Console.WriteLine($"{cliente.Nome} {cliente.Idade}");
+
+        Console.ReadKey();
+    }
+}
+
+public struct Cliente
+{
+    private string? nome;
+
+    public string? Nome { get => nome; set => nome = value; }
+    public int Idade { get; set; }
+
+    public Cliente(string? nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
